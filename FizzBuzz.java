9e3091efb8c3578ed529c236626fe213f619b0a4
VaishuG <vaishalighay123@gmail.com>
2017-08-09 15:07:21 +0530

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,27 @@
+/*program for fizz and buzz in java*/
+public class Sample
+{
+public static void main(String args[])
+{
+int no;
+for(int i=0;i<no;i++)
+{
+if(i%3==0 && i%5==0)
+{
+System.out.println("Fizz Buzz");
+}
+else if(i%5==0)
+{
+System.out.println("fizz");
+}
+else if(i%3==0)
+{
+System.out.println("Buzz");
+}
+else
+{
+System.out.println(" ");
+}
+}
+}
+}
